9070db8fb206a427b78cebf65a3bf8f65c6bcffe
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2023-05-22 11:56:58 +0530

    pattern questions upto lecture 4

---
+++ patterns/pattern2.cpp
@@ -0,0 +1,78 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+#include<bits/stdc++.h>
+using namespace std;
+#define bit(x,i) (x&(1<<i))
+#define lowbit(x) ((x)&((x)^((x)-1))) 
+#define hBit(msb,n) asm("bsrl %1,%0" : "=r"(msb) : "r"(n))
+#define max(a,b) (a<b?b:a)
+#define abs(x) (x<0?(-x):x)
+#define IN(i,l,r) (l<i&&i<r)
+#define LINR(i,l,r) (l<=i&&i<=r)
+#define LIN(i,l,r) (l<=i&&i<r)
+#define INR(i,l,r) (l<i&&i<=r)
+#define F(i,L,R) for (int i = L; i < R; i++) 
+#define FE(i,L,R) for (int i = L; i <= R; i++)
+#define FF(i,L,R) for (int i = L; i > R; i--)
+#define FFE(i,L,R) for (int i = L; i >= R; i--)
+#define getI(a) scanf("%d", &a)
+#define getII(a,b) scanf("%d%d", &a, &b)
+#define getIII(a,b,c) scanf("%d%d%d", &a, &b, &c)
+#define wez(n) int (n); scanf("%d",&(n)) 
+#define wez2(n,m) int (n),(m); scanf("%d %d",&(n),&(m))
+#define wez3(n,m,k) int (n),(m),(k); scanf("%d %d %d",&(n),&(m),&(k))
+#define TESTS wez(testow)while(testow--)
+#define whileZ int T; getI(T); while(T--) // the same as above
+#define getS(x) scanf("%s", x)
+#define clr(a,x) memset(a,x,sizeof(a)) 
+#define char2Int(c) (c-'0')
+#define lastEle(vec) vec[vec.size()-1] 
+#define SZ(x) ((int)((x).size()))
+#define REMAX(a,b) (a)=max((a),(b))
+#define REMIN(a,b) (a)=min((a),(b));
+#define FOREACH(i,t) for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define ALL(c) (c).begin(),(c).end()
+#define PRESENT(c,x) ((c).find(x) != (c).end()) 
+#define CPRESENT(c,x) (find(ALL(c),x) != (c).end()) 
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+#define IOS ios_base::sync_with_stdio(0);
+#define INF 1001001001
+#define PI 3.1415926535897932384626
+#define mp make_pair
+#define fi first
+#define se second
+inline void pisz(int n) { printf("%d\n",n); }
+#define DBG(vari) cerr<<#vari<<" = "<<(vari)<<endl;
+#define printA(a,L,R) FE(i,L,R) cout << a[i] << (i==R?'\n':' ')
+#define printV(a) printA(a,0,a.size()-1)
+#define MAXN 10000
+#define pb push_back
+typedef int elem_t;
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii; 
+const int fx[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+const int fxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+template<typename T,typename TT> ostream& operator<<(ostream &s,pair<T,TT> t) {return s<<"("<<t.first<<","<<t.second<<")";}
+template<typename T> ostream& operator<<(ostream &s,vector<T> t){F(i,0,SZ(t))s<<t[i]<<" ";return s; }
+int main()
+{
+    int n;
+    cin>>n;
+    int r=1;
+    while(r<=n){
+        int col=r;
+        while(col){
+            cout<<col<<" ";
+            col--;
+        }
+        cout<<endl;
+        r++;
+    }
+    return 0;
+}
